187544147b576b15147dd2eb6732bf15d6adb9dd
Pandu Galang <pandu4431@gmail.com>
2019-02-28 20:52:17 +0700

    INI APAAN SIH

--- LatOJDBC/src/views/DeptTest.java
+++ LatOJDBC/src/views/DeptTest.java
@@ -21,10 +21,10 @@ public class DeptTest extends javax.swing.JFrame {
     public DeptTest() {
         initComponents();
 //        getMiddle();
-        DepartmentView ev = new DepartmentView();
-        jPanel1.add(ev);
-        ev.setVisible(true);
-        ev.revalidate();
+        DepartmentView dv = new DepartmentView();
+        departmentViewHolder.add(dv);
+        dv.setVisible(true);
+        dv.revalidate();
     }
 //    
 //    private void getMiddle() {
@@ -41,30 +41,35 @@ public class DeptTest extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        departmentViewHolder = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+        javax.swing.GroupLayout departmentViewHolderLayout = new javax.swing.GroupLayout(departmentViewHolder);
+        departmentViewHolder.setLayout(departmentViewHolderLayout);
+        departmentViewHolderLayout.setHorizontalGroup(
+            departmentViewHolderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 714, Short.MAX_VALUE)
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+        departmentViewHolderLayout.setVerticalGroup(
+            departmentViewHolderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 467, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(departmentViewHolder, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(departmentViewHolder, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -106,6 +111,6 @@ public class DeptTest extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel departmentViewHolder;
     // End of variables declaration//GEN-END:variables
 }
